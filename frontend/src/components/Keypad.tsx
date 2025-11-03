94a0c43982a33b138dd3c745900340ba151ece04
ocsoriram <ossoriram@gmail.com>
2025-11-03 21:33:16 +0900

    feat: keypadの配列を修正
    文字の種類でボタンの背景色を変更させる予定

--- frontend/src/components/Keypad.tsx
+++ frontend/src/components/Keypad.tsx
@@ -1,20 +1,44 @@
 
-const keys = [
-  "(",")","%","AC",
-  "7","8","9","÷",
-  "4","5","6","×",
-  "1","2","3","-",
-  "0",".","=","+",
+type ButtonConfig = {
+  label: string,
+  type: 'btnNum' | 'btnOperator' | 'btnFunc' | 'btnOther';
+}
+
+const buttonConfigs: ButtonConfig[] = [
+  {label: "(", type: "btnFunc"},
+  {label: ")", type: "btnFunc"},
+  {label: "%", type: "btnOperator"},
+  {label: "AC", type: "btnFunc"},
+
+  {label: "7", type: "btnNum"},
+  {label: "8", type: "btnNum"},
+  {label: "9", type: "btnNum"},
+  {label: "÷", type: "btnOperator"},
+
+  {label: "4", type: "btnNum"},
+  {label: "5", type: "btnNum"},
+  {label: "6", type: "btnNum"},
+  {label: "×", type: "btnOperator"},
+
+  {label: "1", type: "btnNum"},
+  {label: "2", type: "btnNum"},
+  {label: "3", type: "btnNum"},
+  {label: "-", type: "btnOperator"},
+
+  {label: "0", type: "btnNum"},
+  {label: ".", type: "btnOther"},
+  {label: "=", type: "btnOperator"},
+  {label: "+", type: "btnOperator"},
 ];
 
 type Props = {onPress: (key: string) => void; disabled?: boolean };
 
 export function Keypad({onPress, disabled}: Props) {
   return (
-    <div className="keypad grid grid-cols-4 gap-2 p-2">
-      {keys.map((k) => (
-        <button key={k} onClick={() => onPress(k)} disabled={disabled} aria-label={k}>
-          {k}
+    <div className="keypad">
+      {buttonConfigs.map((btn) => (
+        <button className={btn.type} key={btn.label} onClick={() => onPress(btn.label)} disabled={disabled} aria-label={btn.label}>
+          {btn.label}
         </button>
        ))}
     </div>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import "./index.css";
 import './input.css';
+import './keypad.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
